9175fe42c96e90d2816a9269ab580aeae6aa4daf
Harshit Jain <nishajain6751@gmail.com>
2016-09-12 13:29:28 +0530

    fix kgsl conflict

--- drivers/gpu/msm/kgsl_sync.c
+++ drivers/gpu/msm/kgsl_sync.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 /* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
-=======
-/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
->>>>>>> cee40b0... msm: kgsl: Avoid race condition in ioctl_syncsource_destroy
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
